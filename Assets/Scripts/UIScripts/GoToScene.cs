0ef904b9a615281d3bcc7f12ad8cfea93f2cf5e8
Jordan Culver <Jordan6372927@gmail.com>
2017-10-30 18:55:00 -0400

    Basic template of main menu and character selection. Need to fill out with art assets and actual gameplay.

---
+++ Assets/Scripts/UIScripts/GoToScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour {
+
+	public string sceneName;
+
+	public void LoadScene()
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public void LoadScene(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+	public void LoadSceneAsync()
+	{
+		SceneManager.LoadSceneAsync(sceneName);
+	}
+
+	public void LoadSceneAsync(string scene)
+	{
+		SceneManager.LoadSceneAsync(scene);
+	}
+}
