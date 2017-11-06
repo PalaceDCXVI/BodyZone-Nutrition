9f89b77151810d483a79039a59184bf244d2b35a
Palace <Jordan6372927@gmail.com>
2017-11-06 17:06:02 -0500

    Added Module selection and popups

---
+++ Assets/Scripts/UIScripts/PopupScripts/Popup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour {
+
+	public Text TitleText;
+	public Text PopupDescription;
+	public Button CancelButton;
+	public Button AcceptButton;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void SetTitleText(string titleText)
+	{
+		TitleText.text = titleText.Replace("\\n", "\n");
+	}
+
+	public void SetDescription(string descriptionText)
+	{
+		PopupDescription.text = descriptionText.Replace("\\n", "\n");
+	}
+
+	public void DestroyPopup()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/UIScripts/PopupScripts/PopupSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupSpawner : MonoBehaviour {
+
+	public GameObject popupObject;
+
+	Popup popup;
+
+	public string titleText = "Title Text";
+	public string descriptionText = "Popup Description";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnPopup()
+	{
+		popup = Instantiate(popupObject).GetComponent<Popup>();
+		popup.SetTitleText(titleText);
+		popup.SetDescription(descriptionText);
+	}
+}
