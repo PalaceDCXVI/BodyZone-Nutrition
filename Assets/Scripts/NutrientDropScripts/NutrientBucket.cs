0f864ff1c84442d429fa13a44f1d774656ac4f97
Unknown <robert.savaglio@uoit.net>
2017-12-12 11:54:15 -0500

    [MODIFY]
    - Change movement to which side of the robot is pressed not, side of the screen
    - Changed music

--- Assets/Scripts/NutrientDropScripts/NutrientBucket.cs
+++ Assets/Scripts/NutrientDropScripts/NutrientBucket.cs
@@ -24,8 +24,8 @@ public class NutrientBucket : MonoBehaviour {
 		// Robot's Movement, based on Rigid body velocity and which side of the screen is clicked
 		if (Input.GetMouseButton(0))
  		{
-     		// If left side of the screen is clicked
-     		if (Input.mousePosition.x < Screen.width/2)
+     		// When clicking to the left of the robot
+     		if (Input.mousePosition.x < rectTransform.position.x)
      		{
 				 Debug.Log ("Clicked Left Side");
       			// Move robot left and slightly tilt
@@ -33,8 +33,8 @@ public class NutrientBucket : MonoBehaviour {
 				GetComponent<Rigidbody2D>().rotation = 20f;
 
      		}
-			 // If right side of the screen is clicked
-     		else if (Input.mousePosition.x > Screen.width/2)
+			// When clicking to the right of the robot
+     		else if (Input.mousePosition.x > rectTransform.position.x)
      		{
 				Debug.Log ("Clicked Right Side");
         		// Move Robot Right and slightly tilt
@@ -44,7 +44,7 @@ public class NutrientBucket : MonoBehaviour {
  		}
 		else
 		{
-			// Slow Robot when there is not input, and reset rotation
+			// Slow Robot when there is no input, and reset rotation
 			GetComponent<Rigidbody2D>().AddForce(new Vector2 (-GetComponent<Rigidbody2D>().velocity.x * 2,0));
 			GetComponent<Rigidbody2D>().rotation = 0f;
 		}
