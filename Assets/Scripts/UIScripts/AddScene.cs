68d2f1170bee42c87c6ba50091648863d5548304
Jordan Culver <Jordan6372927@gmail.com>
2018-01-12 22:58:41 -0500

    Added food database as an overlay scene.

---
+++ Assets/Scripts/UIScripts/AddScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AddScene : MonoBehaviour {
+
+	public void Add(string sceneName)
+	{
+		SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+	}
+
+	public void Remove(string sceneName)
+	{
+		SceneManager.UnloadSceneAsync(sceneName);
+	}
+}
